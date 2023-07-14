5b394470642b0652c87c3eec48720ec527386d0c
Aly-Ant0 <alyssonantoniogamers@gmail.com>
2023-07-14 03:12:17 -0300

    gshhsvahabajahahhggsgsgsgah

--- source/Main.hx
+++ source/Main.hx
@@ -22,37 +22,9 @@ class Main extends Sprite
 	];
 	*/
 
-	public static function main()
-	{
-		Lib.current.addChild(new Main());
-	}
-
 	public function new()
 	{
 		super();
-
-		if (stage != null)
-		{
-			init();
-		}
-		else
-		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
-	}
-
-	private function init(?E:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
-			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
-
-		setupGame();
-	}
-
-	private function setupGame():Void
-	{
-		return addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, false));
+		addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, false));
 	}
 }
